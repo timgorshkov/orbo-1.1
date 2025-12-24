10f93f6bbb1bbcf124c4311046d4c700fce65734
Tim Gorshkov <timfreelancer@gmail.com>
2025-12-24 21:45:17 +0300

    счетчик метрики

--- app/site/layout.tsx
+++ app/site/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from 'next';
+import Script from 'next/script';
 import '@/components/website/website.css';
 
 export const metadata: Metadata = {
@@ -23,6 +24,9 @@ export const metadata: Metadata = {
   },
 };
 
+// Yandex.Metrika counter ID
+const YM_COUNTER_ID = 104139201;
+
 export default function WebsiteLayout({
   children,
 }: {
@@ -31,6 +35,40 @@ export default function WebsiteLayout({
   return (
     <div className="website-root">
       {children}
+      
+      {/* Yandex.Metrika counter */}
+      <Script
+        id="yandex-metrika"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            (function(m,e,t,r,i,k,a){
+              m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+              m[i].l=1*new Date();
+              for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+              k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+            })(window, document, 'script', 'https://mc.yandex.ru/metrika/tag.js?id=${YM_COUNTER_ID}', 'ym');
+
+            ym(${YM_COUNTER_ID}, 'init', {
+              ssr: true,
+              webvisor: true,
+              clickmap: true,
+              ecommerce: 'dataLayer',
+              accurateTrackBounce: true,
+              trackLinks: true
+            });
+          `,
+        }}
+      />
+      <noscript>
+        <div>
+          <img 
+            src={`https://mc.yandex.ru/watch/${YM_COUNTER_ID}`} 
+            style={{ position: 'absolute', left: '-9999px' }} 
+            alt="" 
+          />
+        </div>
+      </noscript>
     </div>
   );
 }
